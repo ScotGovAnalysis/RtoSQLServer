9d7acd82adc0ec32af73d2aee36ef6dec2074b2e
tomwilsonsco <thomaswilson81@gmail.com>
2022-05-13 20:46:09 +0100

    system versioning table drop

--- R/drop_table.R
+++ R/drop_table.R
@@ -1,5 +1,11 @@
 drop_table_from_db <- function(database, server, schema, table_name) {
-  sql <- paste0("DROP TABLE [", schema, "].[", table_name, "]")
-  execute_sql(database = database, server = server, sql = sql, output = TRUE)
+  drop_sql <- list(
+    paste0("ALTER TABLE [", schema, "].[", table_name, "] SET ( SYSTEM_VERSIONING = OFF )"),
+    paste0("DROP TABLE [", schema, "].[", table_name, "]"),
+    paste0("DROP TABLE [", schema, "].[", table_name, "History]")
+  )
+  for (sql in drop_sql) {
+    execute_sql(database = database, server = server, sql = sql, output = TRUE)
+  }
   message("Table: '", table_name, "' successfully deleted from database: '", database, "' on server '", server, "'")
 }
