f66ade04f93f1e686d2061a77228eebd777338ce
tomwilsonsco <thomaswilson81@gmail.com>
2022-05-16 09:32:29 +0100

    doc for add table

--- R/create_replace_table.R
+++ R/create_replace_table.R
@@ -19,12 +19,15 @@ populate_staging_table <- function(database, server, schema, table, dataframe, o
     create_staging_table(database = database, server = server, schema = schema, table = table, dataframe = dataframe)
     overwrite <- TRUE
   }
-  tryCatch({
-    DBI::dbWriteTable(connection, name = DBI::Id(schema = schema, table = paste0(table, "_staging_")), value = dataframe, overwrite = overwrite, append = append)
-    message(paste0("Staging successfully written to database"))
-  }, error = function(cond) {
-    stop(paste0("Failed to write staging data to database.\nOriginal error message: ", cond))
-  })
+  tryCatch(
+    {
+      DBI::dbWriteTable(connection, name = DBI::Id(schema = schema, table = paste0(table, "_staging_")), value = dataframe, overwrite = overwrite, append = append)
+      message(paste0("Staging successfully written to database"))
+    },
+    error = function(cond) {
+      stop(paste0("Failed to write staging data to database.\nOriginal error message: ", cond))
+    }
+  )
   DBI::dbDisconnect(connection)
 }
 
@@ -47,11 +50,14 @@ populate_table_from_staging <- function(database, server, schema, table) {
 
 delete_staging_table <- function(database, server, schema, table) {
   connection <- create_sqlserver_connection(database = database, server = server)
-  tryCatch({
-    odbc::dbRemoveTable(conn = connection, DBI::Id(schema = schema, table = paste0(table, "_staging_")))
-  }, error = function(cond) {
-    stop(paste0("Failed to delete staging table: '", table, "' from database: '", database, "' on server: '", server, "'\nOriginal error message: ", cond))
-  })
+  tryCatch(
+    {
+      odbc::dbRemoveTable(conn = connection, DBI::Id(schema = schema, table = paste0(table, "_staging_")))
+    },
+    error = function(cond) {
+      stop(paste0("Failed to delete staging table: '", table, "' from database: '", database, "' on server: '", server, "'\nOriginal error message: ", cond))
+    }
+  )
   DBI::dbDisconnect(connection)
   message("Staging table: '", table, "' successfully deleted from database: '", database, "' on server '", server, "'")
 }
@@ -66,11 +72,13 @@ create_versioned_table <- function(database, server, schema, table) {
     data_type <- metadata[row, "DataType"]
     sql <- paste0(sql, " [", column_name, "] ", data_type, ", ")
   }
-  sql <- paste0(sql,
-                "SysStartTime DATETIME2 GENERATED ALWAYS AS ROW START NOT NULL, ",
-                "SysEndTime DATETIME2 GENERATED ALWAYS AS ROW END NOT NULL, ",
-                "PERIOD FOR SYSTEM_TIME (SysStartTime, SysEndTime)) ",
-                "WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE = [", schema, "].[", table, "History]));")
+  sql <- paste0(
+    sql,
+    "SysStartTime DATETIME2 GENERATED ALWAYS AS ROW START NOT NULL, ",
+    "SysEndTime DATETIME2 GENERATED ALWAYS AS ROW END NOT NULL, ",
+    "PERIOD FOR SYSTEM_TIME (SysStartTime, SysEndTime)) ",
+    "WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE = [", schema, "].[", table, "History]));"
+  )
   connection <- create_sqlserver_connection(database = database, server = server)
   DBI::dbGetQuery(connection, sql)
   DBI::dbDisconnect(connection)
@@ -78,18 +86,35 @@ create_versioned_table <- function(database, server, schema, table) {
 
 
 
+#' Write an R dataframe to SQL Server table with system versioning
+#'
+#' @param database
+#' @param server
+#' @param schema
+#' @param table_name
+#' @param dataframe
+#'
+#'
+#' @export
+#'
+#' @examples
+#' write_dataframe_to_db(database = "my_database", server = "my_server", schema = "my_schema", table_name = "output_table", dataframe = my_df)
 write_dataframe_to_db <- function(database, server, schema, table_name, dataframe) {
   populate_staging_table(database = database, server = server, schema = schema, table = table_name, dataframe = dataframe)
   connection <- create_sqlserver_connection(database = database, server = server)
   tables <- get_db_tables(database = database, server = server)
   if (nrow(tables[tables$Schema == schema & tables$Name == table_name, ]) == 0) {
-    create_versioned_table(database = database, server = server, schema = schema, table = table_name)}
-  tryCatch({
-    populate_table_from_staging(database = database, server = server, schema = schema, table = table_name)
-    message(paste0("Dataframe successfully written to: '", table_name, "'"))
-  }, error = function(cond) {
-    stop(paste0("Failed to write dataframe to database: '", database, "'\nOriginal error message: ", cond))
-  })
+    create_versioned_table(database = database, server = server, schema = schema, table = table_name)
+  }
+  tryCatch(
+    {
+      populate_table_from_staging(database = database, server = server, schema = schema, table = table_name)
+      message(paste0("Dataframe successfully written to: '", table_name, "'"))
+    },
+    error = function(cond) {
+      stop(paste0("Failed to write dataframe to database: '", database, "'\nOriginal error message: ", cond))
+    }
+  )
   delete_staging_table(database = database, server = server, schema = schema, table = table_name)
   DBI::dbDisconnect(connection)
 }
