3e418e0eb4f0d7af4e68f1e8dbfa0dc5ade0bb68
tomwilsonsco <thomaswilson81@gmail.com>
2022-05-16 08:50:27 +0100

    drop table sql separator

--- R/drop_table.R
+++ R/drop_table.R
@@ -1,8 +1,8 @@
 drop_versioned_table_from_db <- function(database, server, schema, table_name) {
   drop_sql <- list(
-    paste0("ALTER TABLE [", schema, "].[", table_name, "] SET ( SYSTEM_VERSIONING = OFF )"),
-    paste0("DROP TABLE [", schema, "].[", table_name, "]"),
-    paste0("DROP TABLE [", schema, "].[", table_name, "History]")
+    paste0("ALTER TABLE [", schema, "].[", table_name, "] SET ( SYSTEM_VERSIONING = OFF );",
+    "DROP TABLE [", schema, "].[", table_name, "];",
+    "DROP TABLE [", schema, "].[", table_name, "History]")
   )
   for (sql in drop_sql) {
     execute_sql(database = database, server = server, sql = sql, output = TRUE)
