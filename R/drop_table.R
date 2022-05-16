7d398a289cc4665d94c8ddaf4aec5f2a5253db30
tomwilsonsco <thomaswilson81@gmail.com>
2022-05-16 09:36:12 +0100

    drop table doc

--- R/drop_table.R
+++ R/drop_table.R
@@ -1,8 +1,21 @@
+#' Drop versioned SQL Server table from database
+#'
+#' @param database
+#' @param server
+#' @param schema
+#' @param table_name
+#'
+#' @export
+#'
+#' @examples
+#' drop_versioned_table_from_db(database = "my_database", server = "my_server", schema = "my_schema", table_name = "table_to_drop")
 drop_versioned_table_from_db <- function(database, server, schema, table_name) {
   drop_sql <- list(
-    paste0("ALTER TABLE [", schema, "].[", table_name, "] SET ( SYSTEM_VERSIONING = OFF );",
-    "DROP TABLE [", schema, "].[", table_name, "];",
-    "DROP TABLE [", schema, "].[", table_name, "History]")
+    paste0(
+      "ALTER TABLE [", schema, "].[", table_name, "] SET ( SYSTEM_VERSIONING = OFF );",
+      "DROP TABLE [", schema, "].[", table_name, "];",
+      "DROP TABLE [", schema, "].[", table_name, "History]"
+    )
   )
   for (sql in drop_sql) {
     execute_sql(database = database, server = server, sql = sql, output = TRUE)
