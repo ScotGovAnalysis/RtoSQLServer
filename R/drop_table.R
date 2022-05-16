1913a378349715b99346d56a5586200a7b6bfe58
tomwilsonsco <thomaswilson81@gmail.com>
2022-05-16 15:23:51 +0100

    drop table function tidy

--- R/drop_table.R
+++ R/drop_table.R
@@ -10,15 +10,11 @@
 #' @examples
 #' drop_versioned_table_from_db(database = "my_database", server = "my_server", schema = "my_schema", table_name = "table_to_drop")
 drop_versioned_table_from_db <- function(database, server, schema, table_name) {
-  drop_sql <- list(
-    paste0(
-      "ALTER TABLE [", schema, "].[", table_name, "] SET ( SYSTEM_VERSIONING = OFF );",
-      "DROP TABLE [", schema, "].[", table_name, "];",
-      "DROP TABLE [", schema, "].[", table_name, "History]"
-    )
+  drop_sql <- paste0(
+    "ALTER TABLE [", schema, "].[", table_name, "] SET ( SYSTEM_VERSIONING = OFF );",
+    "DROP TABLE [", schema, "].[", table_name, "];",
+    "DROP TABLE [", schema, "].[", table_name, "History]"
   )
-  for (sql in drop_sql) {
-    execute_sql(database = database, server = server, sql = sql, output = TRUE)
-  }
+  execute_sql(database = database, server = server, sql = drop_sql, output = TRUE)
   message("Table: '", table_name, "' successfully deleted from database: '", database, "' on server '", server, "'")
 }
