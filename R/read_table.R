a3e48513f65693a4743135b4c6b3ecd13218bf69
tomwilsonsco <thomaswilson81@gmail.com>
2022-09-23 10:08:18 +0100

    check table already exists

--- R/read_table.R
+++ R/read_table.R
@@ -12,7 +12,9 @@
 #' @examples
 #' read_table_from_db(database = "my_database", server = "my_server", table_name = "my_table", columns = c("column1", "column2"))
 read_table_from_db <- function(database, server, schema, table_name, columns = NULL) {
-  id_column <- paste0(table_name, "ID")
+  tables <- get_db_tables(database = database, server = server)
+  if (nrow(tables[tables$Schema == schema & tables$Name == table_name, ]) == 0) {
+    stop(paste0("Table '", schema, ".", table_name, "' does not exist in the database."))}
   select_list <- table_select_list(columns)
   sql <- paste0(
     "SELECT ", select_list,
