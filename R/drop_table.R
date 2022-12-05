963cea1f8fd001fb091d5e3cdec10c24dba8a63d
tomwilsonsco <thomaswilson81@gmail.com>
2022-12-05 15:48:51 +0000

    refactor from test dev

--- R/drop_table.R
+++ R/drop_table.R
@@ -1,3 +1,73 @@
+# check table sql
+create_check_sql <- function(schema, table_name) {
+  paste0("select name, temporal_type_desc
+          from sys.tables where name = '", table_name, "'
+          and schema_name(schema_id) = '", schema, "'")
+}
+
+
+# versioned table sql
+create_drop_sql_versioned <- function(schema, table_name) {
+  paste0(
+    "ALTER TABLE [", schema, "].[", table_name, "]
+     SET ( SYSTEM_VERSIONING = OFF );",
+    "DROP TABLE [", schema, "].[", table_name, "];",
+    "DROP TABLE [", schema, "].[", table_name, "History]"
+  )
+}
+
+# non versioned table sql
+create_drop_sql_nonversioned <- function(schema, table_name) {
+  paste0("DROP TABLE [", schema, "].[", table_name, "]")
+}
+
+# Derive the drop sql depending on if versioned (check first)
+create_drop_sql <- function(server,
+                            database,
+                            schema,
+                            table_name,
+                            versioned_table) {
+  if (versioned_table) {
+
+    # Check is versioned table regardless of versioned_table input arg
+    check_sql <- create_check_sql(schema, table_name)
+    check_df <- execute_sql(
+      server = server,
+      database = database,
+      sql = check_sql,
+      output = TRUE
+    )
+    if (check_df[["temporal_type_desc"]] ==
+      "SYSTEM_VERSIONED_TEMPORAL_TABLE") {
+      drop_sql <- create_drop_sql_versioned(schema, table)
+    }
+    # In case use the versioned_table argument as TRUE but not versioned table
+    else {
+      drop_sql <- create_drop_sql_nonversioned(schema, table)
+    }
+  }
+  # If versioned_table argument is FALSE
+  else {
+    drop_sql <- create_drop_sql_nonversioned(schema, table)
+  }
+  return(drop_sql)
+}
+
+# drop message
+get_drop_message <- function(database,
+                             server,
+                             schema,
+                             table_name) {
+  paste0(
+    "Table: '",
+    schema, ".",
+    table_name,
+    "' successfully deleted from database: '",
+    database, "' on server '",
+    server, "'"
+  )
+}
+
 #' Drop SQL Server table from database. Check if versioned table and disable
 #' versioning and drop history table too if so.
 #'
@@ -26,58 +96,46 @@ drop_table_from_db <- function(server,
                                table_name,
                                versioned_table = FALSE,
                                silent = FALSE) {
-  tables <- get_db_tables(database = database, server = server)
-  if (nrow(tables[tables$Schema == schema &
-    tables$Name == table_name, ]) == 0) {
+  if (!check_table_exists(
+    server,
+    database,
+    schema,
+    table_name
+  )) {
     stop(format_message(paste0(
       "Table '", schema, ".", table_name,
       "' does not exist in the database."
     )))
   }
-  if (versioned_table) {
-    check_sql <- paste0("select name, temporal_type,
-                        temporal_type_desc
-                        from sys.tables where name = '", table_name, "'")
-    check_df <- execute_sql(
-      server = server,
-      database = database,
-      sql = check_sql,
-      output = TRUE
-    )
-    if (check_df[["temporal_type_desc"]] ==
-      "SYSTEM_VERSIONED_TEMPORAL_TABLE") {
-      drop_sql <- paste0(
-        "ALTER TABLE [", schema, "].[", table_name, "]
-        SET ( SYSTEM_VERSIONING = OFF );",
-        "DROP TABLE [", schema, "].[", table_name, "];",
-        "DROP TABLE [", schema, "].[", table_name, "History]"
-      )
-    }
-    # In case use the versioned_table argument as TRUE but not versioned table
-    else {
-      drop_sql <- paste0("DROP TABLE
-                         [", schema, "].[", table_name, "]")
-    }
-  }
-  # If versioned_table argument is FALSE
-  else {
-    drop_sql <- paste0("DROP TABLE
-                       [", schema, "].[", table_name, "]")
-  }
-  execute_sql(
-    server = server,
-    database = database,
-    sql = drop_sql,
-    output = TRUE
+
+  # Create drop sql
+
+  drop_sql <- create_drop_sql(
+    server,
+    database,
+    schema,
+    table_name,
+    versioned_table
   )
+
+  # Run drop sql
+
+  execute_sql(
+    server,
+    database,
+    drop_sql,
+    output = FALSE
+  )
+
+  # Output message if required
   if (!silent) {
-    message(format_message(paste0(
-      "Table: '",
-      schema, ".",
-      table_name,
-      "' successfully deleted from database: '",
-      database, "' on server '",
-      server, "'"
-    )))
+    show_message <- get_drop_message(
+      server,
+      database,
+      schema,
+      table_name
+    )
+
+    message(format_message(show_message))
   }
 }
