174c79a3d5d33ef67f460d785407215e167c5a2d
tomwilsonsco <thomaswilson81@gmail.com>
2022-12-06 14:57:46 +0000

    drop table tests

--- tests/testthat/test-drop_table.R
+++ tests/testthat/test-drop_table.R
@@ -1,3 +1,54 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("table not exist error works", {
+  mockery::stub(drop_table_from_db, "check_table_exists", FALSE)
+  expect_error(drop_table_from_db("test", "test", "test", "test"))
+})
+
+
+test_that("versioned drop sql created correctly", {
+  check_df <- data.frame(
+    temporal_type_desc = "SYSTEM_VERSIONED_TEMPORAL_TABLE"
+  )
+
+  drop_ver_sql <- paste0(
+    "ALTER TABLE [test_schema].[test_tbl] ",
+    "SET ( SYSTEM_VERSIONING = OFF ); ",
+    "DROP TABLE [test_schema].[test_tbl]; ",
+    "DROP TABLE [test_schema].[test_tblHistory];"
+  )
+
+  mockery::stub(create_drop_sql, "execute_sql", check_df)
+  #mockery::stub(create_drop_sql, "is_versioned", TRUE)
+
+  expect_equal(create_drop_sql(
+    "test", "test", "test_schema", "test_tbl", TRUE
+  ), drop_ver_sql)
+})
+
+test_that("non-versioned drop sql created correctly", {
+  check_df <- data.frame(
+    temporal_type_desc = "NOT APPLICABLE"
+  )
+
+  drop_nonver_sql <- "DROP TABLE [test_schema].[test_tbl];"
+
+  mockery::stub(create_drop_sql, "execute_sql", check_df)
+  #mockery::stub(create_drop_sql, "is_versioned", FALSE)
+
+  expect_equal(create_drop_sql(
+    "test", "test", "test_schema", "test_tbl", FALSE
+  ), drop_nonver_sql)
+})
+
+test_that("user error non-versioned drop sql created correctly", {
+  check_df <- data.frame(
+    temporal_type_desc = "NOT APPLICABLE"
+  )
+
+  drop_nonver_sql <- "DROP TABLE [test_schema].[test_tbl];"
+
+  mockery::stub(create_drop_sql, "execute_sql", check_df)
+
+  expect_equal(create_drop_sql(
+    "test", "test", "test_schema", "test_tbl", TRUE
+  ), drop_nonver_sql)
 })
