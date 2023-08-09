b116971726bd82194e2a15be53fddc64998fc5d5
tomwilsonsco <thomaswilson81@gmail.com>
2023-08-09 16:13:37 +0100

    filter process initial

--- R/read_table.R
+++ R/read_table.R
@@ -6,22 +6,55 @@ table_select_list <- function(columns) {
   }
 }
 
-create_read_sql <- function(select_list, schema, table_name) {
-  glue::glue(
+format_filter <- function(server, database, filter_stmt) {
+  connection <- create_sqlserver_connection(
+    server = server,
+    database = database
+  )
+  sql <- dbplyr::translate_sql(!! rlang::parse_expr(filter_stmt), con=connection)
+  DBI::dbDisconnect(connection)
+  sql
+}
+
+create_read_sql <- function(server,
+                            database,
+                            schema,
+                            select_list,
+                            table_name,
+                            filter_stmt) {
+  initial_sql <- glue::glue(
     "SELECT {select_list}",
-    "FROM [{schema}].[{table_name}];",
+    "FROM [{schema}].[{table_name}]",
     .sep = " "
   )
+  if (!is.null(filter_stmt)) {
+    filter_stmt <- format_filter(server, database, filter_stmt)
+    glue::glue(
+      initial_sql,
+      "WHERE {filter_stmt};",
+      .sep = " "
+    )
+  } else {
+    glue::glue(initial_sql, ";")
+  }
 }
 
 
-#' Read a SQL Server table into an R dataframe,
+#' Read a SQL Server table into an R dataframe.
+#'
+#' If you are confident in writing SQL you may prefer to
+#' Use \link[RtoSQLServer]{execute_sql} function instead.
+#'
 #'
 #' @param server Server instance where SQL Server database running.
 #' @param database Database containing table to read.
 #' @param schema Name of database schema containing table to read.
 #' @param table_name Name of table in database to read.
 #' @param columns Optional vector of column names to select.
+#' @param filter_stmt Optional filter statement - this should be a character
+#' expression in the format of a `dplyr::filter()` query,
+#' for example `"Species == "virginica"` and it will be translated to SQL
+#' using \link[dbplyr]{translate_sql}.
 #'
 #' @return Dataframe of table.
 #' @export
@@ -32,14 +65,16 @@ create_read_sql <- function(select_list, schema, table_name) {
 #'   database = "my_database",
 #'   server = "my_server",
 #'   table_name = "my_table",
-#'   columns = c("column1", "column2")
+#'   columns = c("column1", "column2"),
+#'   filter_stmt = "column1 < 5 & column2 == 'b'")
 #' )
 #' }
 read_table_from_db <- function(database,
                                server,
                                schema,
                                table_name,
-                               columns = NULL) {
+                               columns = NULL,
+                               filter_stmt = NULL) {
   if (!check_table_exists(
     server,
     database,
@@ -53,11 +88,14 @@ read_table_from_db <- function(database,
   select_list <- table_select_list(columns)
 
   read_sql <- create_read_sql(
-    select_list,
+    server,
+    database,
     schema,
-    table_name
+    select_list,
+    table_name,
+    filter_stmt
   )
-
+  message(glue::glue("Read SQL statement:\n{read_sql}"))
   execute_sql(
     database = database, server =
       server, sql = read_sql, output = TRUE
