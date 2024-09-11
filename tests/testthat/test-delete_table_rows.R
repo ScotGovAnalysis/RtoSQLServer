78dc5d0e83216ff30eebb113e0ea40fe82d2bca6
tomwilsonsco <thomaswilson81@gmail.com>
2024-09-11 13:58:29 +0100

    new delete rows test

---
+++ tests/testthat/test-delete_table_rows.R
@@ -0,0 +1,13 @@
+test_that("filtered delete sql works", {
+  test_sql <- glue::glue(
+    "DELETE FROM \"test_schema\".\"test_tbl\" \\
+     WHERE \"val_col\" = 'a';"
+  )
+
+  initial_sql <- delete_sql("test_schema", "test_tbl")
+
+  filter_stmt = "val_col == 'a'"
+
+
+  expect_equal(add_filter_sql(initial_sql, filter_stmt), test_sql)
+})
