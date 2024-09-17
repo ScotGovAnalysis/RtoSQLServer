4ed24336b340bc016fee761a4cefc5fc7b31a8b9
tomwilsonsco <thomaswilson81@gmail.com>
2024-09-17 10:29:32 +0100

    fix tests

---
+++ tests/testthat/test-drop_column.R
@@ -0,0 +1,11 @@
+test_that("drop col sql works", {
+  test_sql <- glue::glue_sql(
+    "ALTER TABLE \"test_schema\".\"test_tbl\" \\
+     DROP COLUMN \"val_col\";"
+  )
+
+
+  expect_equal(create_drop_column_sql("test_schema",
+                                      "test_tbl",
+                                      "val_col"), test_sql)
+})
