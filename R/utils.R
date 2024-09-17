2c70e941ed91a7bc1196217f2bc0db13974cb875
tomwilsonsco <thomaswilson81@gmail.com>
2024-09-17 10:53:24 +0100

    fix comment

--- R/utils.R
+++ R/utils.R
@@ -40,7 +40,7 @@ df_to_metadata <- function(dataframe) {
     column_name = names(col_types), data_type = unname(col_types),
     stringsAsFactors = FALSE
   )
-  # db_table_metdata stored procedure does not specify size of datetime2 cols
+  # db_table_metdata does not specify size of datetime2 cols
   df[df$data_type == "datetime2(3)", "data_type"] <- "datetime2"
   df
 }
